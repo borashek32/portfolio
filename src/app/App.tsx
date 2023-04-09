00197b4c8d1f9f8855a335958518a3749acd2987
borashek <borashek@inbox.ru>
2023-04-09 12:06:58 +0300

    1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import {Route, Routes} from "react-router-dom";
 import {Telegram} from "components/telegram/Telegram";
 import {Main} from "components/blocks/Main";
 import {Counters} from "components/counters/Counters";
-import {PdfViewerComponent} from "components/blocks/HireMe/CV";
 
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
       <Route path="/" element={<Main name={"React-developer portfolio"}/>} />
       <Route path="/telegram" element={<Telegram name={"Messenger"}/>} />
       <Route path="/counters/*" element={<Counters/>} />
-      <Route path="/cv" element={<PdfViewerComponent/>} />
+      {/*<Route path="/cv" element={<PdfViewerComponent/>} />*/}
     </Routes>
   );
 }
